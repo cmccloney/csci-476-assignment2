929a603c2a621de186f0aab96736e0b0d282caeb
Conner McCloney <cmccloney@hotmail.com>
2019-01-27 15:59:07 -0700

    update

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Main {
 
 	public static void main(String args[]) {
-		String filename = "";
+		String filename = "memorydump.dmp";
 		String line = null;
 		String lines[] = new String[2];
 		int i = 0;
@@ -26,21 +26,65 @@ public class Main {
 		catch(IOException ex) {
 			System.out.println("Error reading file");
 		}
-		//print out 2 lines from .dmp file
-		for(int j = 0; j < 2; j++) {
-			//System.out.println(lines[j]);
-		}
 		
 		processData(lines);
 	}
 	
 	public static void processData(String lines[]) {
-		Pattern p = Pattern.compile("\\^[a-zA-Z\\/]+\\^");
-		Matcher m = p.matcher(lines[0]);
-		if(m.find()) {
-			String name = (m.group(0)); // ^Binhai/Zhu^
+		String[] trackIdata = new String[4];
+		Pattern p0 = Pattern.compile("\\%[0-9a-zA-Z^\\/]*\\?"); //used for finding the track I records
+		Matcher m0 = p0.matcher(lines[0]);
+		int numTracks = 0;
+		while(m0.find()) {
+			trackIdata[numTracks] = m0.group(0);
+			numTracks++;
 		}
-    
+		m0 = p0.matcher(lines[1]);
+		while(m0.find()) {
+			trackIdata[numTracks] = m0.group(0);
+			numTracks++;
+		}
+		
+		/*for(int i = 0; i < numTracks; i++) { 			//print out track I records
+			System.out.println(trackIdata[i]);
+		}*/
+		
+		Pattern p = Pattern.compile("\\^[a-zA-Z\\/]+\\^"); //used for finding the names
+		Matcher m1 = p.matcher(lines[0]);
+		Matcher m2 = p.matcher(lines[1]);
+		String name1 = "", name2 = "";
+		if(m1.find()) {
+			name1 = (m1.group(0)); // ^Binhai/Zhu^
+		}
+		if(m2.find()) {
+			name2 = (m2.group(0)); // ^Paxton/John^
+		}
+		
+		Pattern p2 = Pattern.compile("\\^[0-9a-zA-Z\\/\\^]+\\?\\;[0-9]*=[0-9][0-9][0-9][0-9]"); //used for finding expiration date, I think?
+		Matcher a = p2.matcher(lines[1]);
+		String[] expDate = new String[2];
+		int i = 0;
+		while(a.find()) {
+			expDate[i] = (a.group(0));
+			i++;
+		}
+		
+		//more code
+		
+		System.out.println("There are 2 useable track I records in the memory data");
+		System.out.println("<Information of the 1st record>");
+		System.out.println("Cardholder's Name: " + name1.replace("^", ""));
+		System.out.println("Card Number:");
+		System.out.println("Expiration Date: " + expDate[0].substring(expDate[0].length()-2) + "/20" + expDate[0].substring(expDate[0].length()-4,expDate[0].length()-2));
+		System.out.println("CVC Number:");
+		System.out.println();
+		
+		System.out.println("<Information of the 2nd record>");
+		System.out.println("Cardholder's Name: " + name2.replace("^", ""));
+		System.out.println("Card Number:");
+		System.out.println("Expiration Date: " + expDate[1].substring(expDate[1].length()-2) + "/20" + expDate[1].substring(expDate[1].length()-4,expDate[1].length()-2));
+		System.out.println("CVC Number:");
+		
 	}
 	
 	
